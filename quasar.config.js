d813a4f61d7c618ca01766bd2b67987fb1f21a4a
Lucas Campos <lucash_c@yahoo.com.br>
2026-01-31 19:47:28 -0300

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -25,6 +25,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
+       publicPath: '/portfolio2026/',
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
